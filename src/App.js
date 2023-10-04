d28e55d803c6eae1b752ebddba33f44e837f8e66
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-10-04 16:59:33 +0100

    made modifications to the root of my project

--- src/App.js
+++ src/App.js
@@ -2,22 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'Suala'
+  const x = true
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello { name }</h1>
+      <h2>Life is {x ? "Good" : "Bad"}</h2>
     </div>
   );
 }
