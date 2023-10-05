4f8c6391eb3de6bf8a5c57f6f898d45ac5cdc4fc
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-10-05 16:42:03 +0100

    learnt about react props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
-import React from 'react'
+import PropTypes from 'prop-types'
 
-const Header = () => {
+const Header = ({title}) => {
   return (
     <div>
-      
+      <h1>{title}</h1>
     </div>
   )
 }
+Header.defaultProps = {
+  title: 'Task Tracker'
+}
 
+Header.propTypes = {
+  title: PropTypes.string
+}
 export default Header
