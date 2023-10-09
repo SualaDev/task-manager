90d66b29cc1fcfe6a81cccb364e123fc22b8bc3d
SualaDev <81395134+SualaDev@users.noreply.github.com>
2023-10-09 16:33:31 +0100

    added tasks to the task tracker

--- src/App.js
+++ src/App.js
@@ -1,9 +1,50 @@
+import { useState } from 'react'
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30pm",
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: "Life Appointment",
+      day: "Feb 7th at 2:50pm",
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: "Doctors Appointment",
+      day: "Feb 5th at 2:30am",
+      reminder: false,
+    }
+  ]
+)
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id))
+  }
+
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) => 
+        task.id === id ? { ...task, reminder:
+        !task.reminder } : task
+      )
+    )
+  }
   return (
     <div className="container">
       <Header />
+      {tasks.length > 0 ? ( 
+        <Tasks tasks={tasks} onDelete=
+        {deleteTask} onToggle={toggleReminder}  />
+        ) : (
+          'No Tasks Found'
+        )}
     </div>
   );
 }
